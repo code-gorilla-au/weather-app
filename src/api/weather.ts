86d4813f8fd0a48a31f5d9356c77063c5db2b57c
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-03 10:41:02 +1100

    boilerplate

---
+++ src/api/weather.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from "axios";
+
+const API_KEY = process.env.VUE_APP_WEATHER_KEY;
+const API_URL = process.env.VUE_APP_WEATHER_URL;
+
+async function getCurrentWeather(
+  location = "Sydney"
+): Promise<AxiosResponse<Record<string, unknown>>> {
+  const url = `${API_URL}/current.json?key=${API_KEY}&q=${location}&aqi=no`;
+  const response = await axios.get(url);
+  return response;
+}
+
+async function getForecast(
+  location = "Sydney",
+  days = 5
+): Promise<AxiosResponse<Record<string, unknown>>> {
+  const url = `${API_URL}/forecast.json?key=${API_KEY}&q=${location}&days=${days}&aqi=no&alerts=no`;
+  return axios.get(url);
+}
+
+export { getCurrentWeather, getForecast };
