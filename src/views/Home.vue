617d724be35890d21241aec65c7ef11d4ec704d9
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-03 11:18:59 +1100

    more boiler plate and mock data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="home-weather-summary">
+      <div>
+        <h3>Sydney</h3>
+        <h3>18</h3>
+        <p>Cloudy</p>
+      </div>
+      <div>cloudy graphic</div>
+    </div>
+    <div class="home-weather-details">
+      <p>rain 10%</p>
+      <p>wind 5km</p>
+      <p>sunrise 8am</p>
+      <p>sunset 9pm</p>
+    </div>
+    <div class="home-hourly-forecast">
+      <p>8am</p>
+      <p>9am</p>
+      <p>10am</p>
+      <p>11am</p>
+      <p>12noon</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, onMounted, ref } from "vue";
+import { getForecast } from "@/api/weather";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  components: {},
+  setup() {
+    const forecast = ref({});
+    onMounted(async () => {
+      await getForecast("Sydney", 1);
+    });
+    return {
+      forecast,
+    };
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.home-weather-summary {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.home-weather-details {
+  display: flex;
+  justify-content: space-evenly;
+}
+.home-hourly-forecast {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
