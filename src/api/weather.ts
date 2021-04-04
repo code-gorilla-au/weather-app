ff3dc972500e273cb672eaf3b3af4d63a8d1f441
mauricio riquelme <m.riquelme223@gmail.com>
2021-04-04 15:33:20 +1000

    adding wip

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -4,7 +4,7 @@ import { Weather } from "@/lib/weather";
 const API_KEY = process.env.VUE_APP_WEATHER_KEY;
 const API_URL = process.env.VUE_APP_WEATHER_URL;
 
-async function getCurrentWeather(
+export async function getCurrentWeather(
   location = "Sydney"
 ): Promise<AxiosResponse<Record<string, unknown>>> {
   const url = `${API_URL}/current.json?key=${API_KEY}&q=${location}&aqi=no`;
@@ -12,12 +12,10 @@ async function getCurrentWeather(
   return response;
 }
 
-async function getForecast(
+export async function getForecast(
   location = "Sydney",
   days = 5
 ): Promise<AxiosResponse<Weather>> {
   const url = `${API_URL}/forecast.json?key=${API_KEY}&q=${location}&days=${days}&aqi=no&alerts=no`;
   return axios.get(url);
 }
-
-export { getCurrentWeather, getForecast };
